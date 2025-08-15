87cb3d118115a8d358e4e2188ac8d0e8edc8f0a8
Abdo <voip2.yt4@gmail.com>
2025-08-15 11:32:23 +0300

    Delete sessions.py

--- sessions.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def load_sessions():
-    if not os.path.exists('sessions'):
-        os.makedirs('sessions')
-    
-    sessions = [f.split('.')[0] for f in os.listdir('sessions') if f.endswith('.session')]
-    return sessions
